083e6619804ac6dc74b7993c5fee0315dce7e1eb
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2019-08-31 16:02:03 +0200

    Wrote the main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Post } from './interfaces/post';
+import { Observable } from 'rxjs';
+import { PostsService } from './services/posts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'washr';
+  post = new FormGroup({
+    title: new FormControl(null, Validators.required),
+    content: new FormControl(null, Validators.required),
+    image: new FormControl(null)
+  });
+
+  posts: Observable<any>;
+
+  constructor(private db: AngularFirestore, private pS: PostsService) {}
+
+  ngOnInit() {
+    this.posts = this.pS.getAllPosts;
+  }
+
+  onSubmit() {
+    const newPost: Post = {
+      ...this.post.value, // Get information from the form
+      postedBy: '000000', // TODO: replace this with the UID of the authenticated user
+      postedAt: new Date() // Current date
+    };
+    this.pS.createPost(newPost)
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
+  }
+
 }
