18c3312de535f91952793b27769a3c932b4de778
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2019-08-31 15:56:59 +0200

    Added ng-in-viewport

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InViewportModule } from 'ng-in-viewport';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    InViewportModule, // Module for checking if component is in viewport
+    AngularFireModule.initializeApp(environment.firebaseConfig), // Firebase basic module
   ],
   providers: [],
   bootstrap: [AppComponent]
