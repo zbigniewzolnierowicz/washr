74d410cf8d4afa30172866147dbc208d746c2c60
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2019-09-01 20:15:33 +0200

    Added a function that adds a comment counter to the document

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,27 @@
 import * as functions from 'firebase-functions';
+import { initializeApp, firestore } from 'firebase-admin';
 
+initializeApp();
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+export const aggregateComments = functions.firestore
+    .document('posts/{postId}/comments/{commentId}')
+    .onWrite(async (_, context) => {
+      // Get ID of the post
+      const postId = context.params.postId;
+      // Get ref of the post
+      const postRef = firestore().doc(`posts/${postId}`);
+      // Get amount of comments
+      try {
+        const commentsSnapshot = await postRef.collection('comments').orderBy('postedAt', 'desc').get();
+        const commentCount = commentsSnapshot.size;
+        return postRef.update({ commentCount });
+      }
+      catch (err) {
+        console.error(err);
+      }
+});
