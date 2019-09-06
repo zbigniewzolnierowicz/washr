5490d76f4ab67f7967c70141e4ebd240f21ed8a3
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2019-09-06 19:05:29 +0200

    Refactored Firebase function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,20 +8,21 @@ initializeApp();
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
-export const aggregateComments = functions.firestore
-    .document('posts/{postId}/comments/{commentId}')
-    .onWrite(async (_, context) => {
-      // Get ID of the post
-      const postId = context.params.postId;
-      // Get ref of the post
-      const postRef = firestore().doc(`posts/${postId}`);
-      // Get amount of comments
-      try {
-        const commentsSnapshot = await postRef.collection('comments').orderBy('postedAt', 'desc').get();
-        const commentCount = commentsSnapshot.size;
-        return postRef.update({ commentCount });
-      }
-      catch (err) {
-        console.error(err);
-      }
-});
+export let aggregateComments: functions.CloudFunction<any>;
+aggregateComments = functions.firestore
+  .document('posts/{postId}/comments/{commentId}')
+  .onWrite(async (_, context) => {
+    // Get ID of the post
+    const postId = context.params.postId;
+    // Get ref of the post
+    const postRef = firestore().doc(`posts/${postId}`);
+    // Get amount of comments
+    try {
+      const commentsSnapshot = await postRef.collection('comments').orderBy('postedAt', 'desc').get();
+      const commentCount = commentsSnapshot.size;
+      return postRef.update({commentCount});
+    } catch (err) {
+      console.error(err);
+      return new Error(err);
+    }
+  });
