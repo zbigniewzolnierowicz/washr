b621bd0309e8207883d97e79b2637057bcaadd41
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2019-09-06 19:51:34 +0200

    Refactored Firebase function and added a user document generation and deletion function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,7 @@ import * as functions from 'firebase-functions';
 import { initializeApp, firestore } from 'firebase-admin';
 
 initializeApp();
+const db = firestore();
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
@@ -9,13 +10,15 @@ initializeApp();
 //  response.send("Hello from Firebase!");
 // });
 export let aggregateComments: functions.CloudFunction<any>;
+export let generateUserDocument: functions.CloudFunction<any>;
+export let deleteUserDocument: functions.CloudFunction<any>;
 aggregateComments = functions.firestore
   .document('posts/{postId}/comments/{commentId}')
   .onWrite(async (_, context) => {
     // Get ID of the post
     const postId = context.params.postId;
     // Get ref of the post
-    const postRef = firestore().doc(`posts/${postId}`);
+    const postRef = db.doc(`posts/${postId}`);
     // Get amount of comments
     try {
       const commentsSnapshot = await postRef.collection('comments').orderBy('postedAt', 'desc').get();
@@ -23,6 +26,29 @@ aggregateComments = functions.firestore
       return postRef.update({commentCount});
     } catch (err) {
       console.error(err);
-      return new Error(err);
+      return err;
     }
   });
+
+  generateUserDocument = functions.auth.user().onCreate(user => {
+    const userData = {
+      email: user.email,
+      displayName: user.displayName,
+      phoneNumber: user.phoneNumber,
+      photoURL: user.photoURL,
+      bio: '',
+      followerCount: 0,
+      following: []
+    }
+    console.log(userData);
+
+    db.collection('users').doc(user.uid).create(userData)
+      .then(data => { return data })
+      .catch(err => { return err });
+  })
+
+  deleteUserDocument = functions.auth.user().onDelete(user => {
+    db.collection('users').doc(user.uid).delete()
+      .then(data => { return data })
+      .catch(err => { return err });
+  })
