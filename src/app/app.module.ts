3cbb5e726d1ce5efc7503503a1acdf9119d0c0cd
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2019-09-06 20:44:16 +0200

    Added a Login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { MarkdownPipe } from 'src/app/pipes/markdown.pipe';
 import { InViewportModule } from 'ng-in-viewport';
 import { UploadComponent } from './components/upload/upload.component';
 import { PostComponent } from './components/post/post.component';
+import { LoginComponent } from './components/login/login.component';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 
 @NgModule({
@@ -18,7 +19,8 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
     AppComponent,
     MarkdownPipe, // Pipe for Markdown parsing
     UploadComponent,
-    PostComponent
+    PostComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private auth: AngularFireAuth) { }
+
+  ngOnInit() {
+  }
+
+}
