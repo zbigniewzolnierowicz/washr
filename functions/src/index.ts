2f416f5f5f952d3df7dccb05691ae7c388565383
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2019-09-07 20:11:15 +0200

    Added functions denormalizing data

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,6 +9,8 @@ export let addCommentToUserArray: functions.CloudFunction<any>;
 export let deleteCommentFromUserArray: functions.CloudFunction<any>;
 export let generateUserDocument: functions.CloudFunction<any>;
 export let deleteUserDocument: functions.CloudFunction<any>;
+export let addUserDataToComment: functions.CloudFunction<any>;
+export let addUserDataToPost: functions.CloudFunction<any>;
 aggregateComments = functions.firestore
   .document('posts/{postId}/comments/{commentId}')
   .onWrite(async (_, context) => {
@@ -94,4 +96,46 @@ aggregateComments = functions.firestore
     db.collection('users').doc(user.uid).delete()
       .then(data => { return data })
       .catch(err => { return err });
+  })
+
+  addUserDataToComment = functions.firestore.document('posts/{postId}/comments/{commentId}')
+  .onWrite(async change => {
+    try {
+      const beforeData = change.before.data() || { postedBy: null };
+      const userRef = await db.doc(`users/${beforeData.postedBy}`);
+      const userDocument = await userRef.get();
+      const userData = await userDocument.data() || { displayName: null, photoURL: null };
+      return change.after.ref.update({
+        userData: {
+          userID: beforeData.postedBy,
+          userName: userData.displayName,
+          userPhoto: userData.photoURL
+        }
+      })
+    } catch (error) {
+      console.error(error);
+      return error;
+    }
+
+  })
+
+  addUserDataToPost = functions.firestore.document('posts/{postId}')
+  .onWrite(async change => {
+    try {
+      const beforeData = change.before.data() || { postedBy: null };
+      const userRef = await db.doc(`users/${beforeData.postedBy}`);
+      const userDocument = await userRef.get();
+      const userData = await userDocument.data() || { displayName: null, photoURL: null };
+      return change.after.ref.update({
+        userData: {
+          userID: beforeData.postedBy,
+          userName: userData.displayName,
+          userPhoto: userData.photoURL
+        }
+      })
+    } catch (error) {
+      console.error(error);
+      return error;
+    }
+
   })
