91331a877074f8b5b4055feeec56a4e0d73da877
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2019-09-15 20:03:05 +0200

    Moved new post form to the route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,21 +14,13 @@ import { NsfwService } from './services/nsfw.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  @ViewChild('form', { static: false }) form: ElementRef;
   title = 'washr';
   error: string;
   isLoggedIn: boolean;
   showAdult: boolean;
-  post = new FormGroup({
-    title: new FormControl(null, Validators.required),
-    content: new FormControl(null, Validators.required),
-    isNSFW: new FormControl(false),
-    image: new FormControl(null)
-  });
-  progress: number = null;
 
   // tslint:disable-next-line: max-line-length
-  constructor(private pS: PostsService, private upS: FileUploadService, public afAuth: AngularFireAuth, private router: Router, private nsfw: NsfwService) {}
+  constructor(public afAuth: AngularFireAuth, private router: Router, private nsfw: NsfwService) {}
 
   ngOnInit() {
     this.afAuth.user.subscribe(u => u ? this.isLoggedIn = true : this.isLoggedIn = false);
@@ -39,47 +31,7 @@ export class AppComponent implements OnInit {
     this.nsfw.toggleNsfw();
   }
 
-  closeError() {
-    this.error = '';
-  }
-
   logOut() {
     this.afAuth.auth.signOut().then(() => this.router.navigate(['']));
-  }
-
-  async onSubmit() {
-    if (this.afAuth.auth.currentUser != null) {
-      let uploader: { task: any; ref: any; };
-      if (this.post.value.image != null) {
-        uploader = this.upS.uploadImageForPost(this.post.value.image);
-      }
-      const post: Post = { // Object to be posted as a new post
-        ...this.post.value,
-        commentCount: 0,
-        postedAt: new Date(), // Get the date right
-        postedBy: this.afAuth.auth.currentUser.uid
-      };
-      if (this.post.value.image != null) {
-        uploader.task.percentageChanges()
-        .pipe(
-          finalize(() => this.progress = null)
-        )
-        .subscribe(progress => this.progress = progress); // Get the percentage status of the image upload
-        uploader.task.snapshotChanges().pipe(
-        finalize(() => {
-          uploader.ref.getDownloadURL().subscribe(url => { // Get the URL of the uploaded image
-            post.image = url; // Append it to the object responsible for being uploaded
-            this.pS.createPost(post) // Create a new post
-              .catch(err => this.error = err);
-          });
-        })).subscribe();
-      } else {
-        this.pS.createPost(post) // Create a new post
-          .catch(err => this.error = err);
-      }
-      this.form.nativeElement.reset();
-    } else {
-      this.error = 'You need to log in!';
-    }
   }
 }
--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -61,7 +61,7 @@ export class PostComponent implements OnInit {
     const comment: Post = { // Object to be posted as a new reply
       ...this.reply.value,
       commentCount: 0,
-      postedAt: new Date().getTime(), // Get the current date
+      postedAt: new Date(), // Get the current date
       postedBy: this.afAuth.auth.currentUser.uid
     };
     if (this.reply.value.image != null) {
