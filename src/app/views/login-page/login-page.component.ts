28f23b8927f61807137ebffd30c66ff43231dc98
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2019-09-22 16:46:30 +0200

    Added github login

--- src/app/views/login-page/login-page.component.ts
+++ src/app/views/login-page/login-page.component.ts
@@ -9,17 +9,18 @@ import { Router } from '@angular/router';
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
-
   googleProvider = new auth.GoogleAuthProvider();
+  githubProvider = new auth.GithubAuthProvider();
 
-  constructor(private afAuth: AngularFireAuth, private router: Router) { }
+  constructor(private afAuth: AngularFireAuth, private router: Router) {}
   logInWithGoogle() {
     this.afAuth.auth.signInWithPopup(this.googleProvider).then(() => this.router.navigate(['timeline']));
+  }
+  logInWithGithub() {
+    this.afAuth.auth.signInWithPopup(this.githubProvider).then(() => this.router.navigate(['timeline']));
   }
   logInWithSomethingElse() {
     alert('Not implemented yet!');
   }
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
